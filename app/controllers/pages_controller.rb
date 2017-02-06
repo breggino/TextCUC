18edb420beabc3a5f55f2f227b567f2f81a3cdd8
Isaac R <isaacreggino@gmail.com>
2017-02-06 04:18:38 +0000

    Creacion de los controladores de las paginas dinamicas, Home, Explore, Index y Profile.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+# El controlador de las paginas contiene todo el codigo de las paginas en el directorio /Pages
+class PagesController < ApplicationController
+  
+  # Codigo para pages/index
+  def index
+  end
+
+ # Codigo para pages/home
+  def home
+  end
+
+ # Codigo para pages/profile
+  def profile
+  end
+
+ # Codigo para pages/explore
+  def explore
+  end
+  
+
+end
